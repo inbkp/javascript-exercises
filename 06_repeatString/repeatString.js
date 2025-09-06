234d58a0b345d92e2d527edf944711f74cff0742
inbkp <xbayu13@gmail.com>
2025-09-06 18:04:28 +0800

    finishing javascript exercise 01-11

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, time) {
+    let string = "";
+    for (let i = 0; i < time; i++) {
+        string += word ;
+    }
+    if (time < 0) return "ERROR";
+    return string;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+let revers = "";
+for (let i = str.length - 1;i >= 0;i--) {
+    revers += str[i];
+}
 
+return revers;
 };
 
 // Do not edit below this line
