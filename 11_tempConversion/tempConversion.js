38efe162d227f315ccf4c96ef83a2138f2d1e084
inbkp <xbayu13@gmail.com>
2025-09-06 19:15:38 +0800

    Replace the old function with new shorter function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
 const convertToCelsius = function(c) {
-  const celcius = (c - 32) * 5/9;
-  return Math.round(celcius * 10) / 10;
+  //Second try using just using return:
+  return Math.round((c - 32) * (5 / 9) * 10) / 10;
+
+
+  //First try:
+  //const celcius = (c - 32) * 5/9;
+  //return Math.round(celcius * 10) / 10;
 };
 
 const convertToFahrenheit = function(f) {
-  const fahrenheit = f * (9/5) + 32;
-  return Math.round(fahrenheit * 10) / 10;
+  //Second try using just using return:
+  return Math.round((f * (9 / 5) + 32) * 10) / 10;
+
+  //First try:
+  //const fahrenheit = f * (9/5) + 32;
+  //return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
