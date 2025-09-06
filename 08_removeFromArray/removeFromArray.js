33b6fb2194458c7353d6768c599eee9472f98858
inbkp <xbayu13@gmail.com>
2025-09-06 18:52:27 +0800

    Replace the old function (for loop) with .reduce

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,13 +1,17 @@
 const removeFromArray = function(arr, ...args) {
-    let newArr = [];
-    for (let i=0;i<arr.length;i++)
-    {
-        if (!args.includes(arr[i]))
-        {
-            newArr.push(arr[i]);
-        }
-    }
-    return newArr;
+    //Second try using .reduce function:
+    return arr.reduce((acc, current) => { if (!args.includes(current)) {acc.push(current);} return acc;}, []);
+
+    //First try:
+    //let newArr = [];
+    //for (let i=0;i<arr.length;i++)
+    //{
+    //    if (!args.includes(arr[i]))
+    //    {
+    //        newArr.push(arr[i]);
+    //    }
+    //}
+    //return newArr;
 };
 
 // Do not edit below this line
